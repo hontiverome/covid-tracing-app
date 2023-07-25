d7b0464b1df2536c630616077f5503284fed7522
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-07-25 17:15:14 +0800

    more pseudocode

--- main-covid-tracing-app.py
+++ main-covid-tracing-app.py
@@ -20,3 +20,15 @@
     # vaccination status
     # symptoms status
     # contact person details
+# create the gui for search function
+    # search by first name
+    # search by middle name
+    # search by last name
+    # search by age
+    # search by contact info
+    # search by occupation
+    # search by email
+    # search by address
+    # search by vaccination status
+    # search by symptoms status
+    # search by contact person details
