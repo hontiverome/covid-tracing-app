1e9a04450de21c28ea92e1a2ab9edf713300be04
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-07-25 17:12:17 +0800

    pseudocode

---
+++ covid-tracing-app.py
@@ -0,0 +1,22 @@
+# HONTIVEROS, JEROME ANDREI O.
+# BSCPE 1-5
+# main menu
+
+# | FINAL PROJECT |
+
+# pseudocode
+# import tkinter app
+# create window
+# create the gui for each user input regarding
+    # user first name
+    # user middle name
+    # user last name
+    # user age
+    # user contact info
+    # use birth date
+    # user occupation 
+    # user email
+    # user address
+    # vaccination status
+    # symptoms status
+    # contact person details
