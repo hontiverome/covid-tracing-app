e6d269ae7b8aea903310bd1f20dc909ec033d1d4
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-07-26 03:09:45 +0800

    main menu creation

--- main_covid_tracing_app.py
+++ main_covid_tracing_app.py
@@ -15,4 +15,17 @@
 # import modules
 import tkinter
 from tkinter import messagebox, ttk
+# import class
 from covid_form import covidForm
+class mainMenu:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        # title
+        self.window.title("COVID-19 Contact Tracing App")
+        # size
+        self.window.geometry("600x400")
+        # color
+        self.window.configure(bg='#8db5db')
+        # label
+        titleLabel = tkinter.Label(self.window, text="COVID-19 TRACE APP", fg="white" , bg="grey", font=("Century Gothic", 30))
+        titleLabel.pack(pady=60)  
