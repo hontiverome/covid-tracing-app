072d5f2f3f8fe5518f0c9f303ad135bdd63f9ed7
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-07-26 02:57:27 +0800

    renamed

---
+++ main_covid_tracing_app.py
@@ -0,0 +1,18 @@
+# HONTIVEROS, JEROME ANDREI O.
+# BSCPE 1-5
+# main menu
+
+# | FINAL PROJECT |
+
+# pseudocode
+# import modules
+# import class
+# create window
+# create the gui for the main menu
+    # button for the form
+    # search entry
+    
+# import modules
+import tkinter
+from tkinter import messagebox, ttk
+from covid_form import covidForm
