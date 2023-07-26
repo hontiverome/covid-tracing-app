a4cc10c086b06e1c035d14f6d420c9f445ed5d34
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-07-26 22:44:00 +0800

    pseudocode

---
+++ search_function.py
@@ -0,0 +1,16 @@
+# HONTIVEROS, JEROME ANDREI O.
+# BSCPE 1-5
+# covid form
+
+# | FINAL PROJECT |
+
+# pseudocode
+# import modules
+# create window
+# create the gui for the search function, which searches for the user input regarding
+    # first name
+    # middle name
+    # last name
+    # age
+    # contact info
+    # email
